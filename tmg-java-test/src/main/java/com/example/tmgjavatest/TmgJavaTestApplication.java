ec168166899dd909f8404da88ce26d033912c212
Caio Riva <caio.riva@pyyne.com>
2023-03-15 20:15:50 -0300

    Add config files and update some classes

---
+++ tmg-java-test/src/main/java/com/example/tmgjavatest/TmgJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmgjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmgJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmgJavaTestApplication.class, args);
+    }
+
+}
---
+++ tmg-java-test/src/test/java/com/example/tmgjavatest/TmgJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmgjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmgJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
